7165ab65f1c8a7e7a2ec6d5ef9158fab3e3ffb91
eesoymilk <qiyoudaoyi@gmail.com>
2024-01-14 00:49:24 +0800

    matching...

--- extras/as608_gui/fingerprint_matcher.py
+++ extras/as608_gui/fingerprint_matcher.py
@@ -1,33 +1,45 @@
-from typing import Optional
+from typing import Sequence
 
 import numpy as np
 import cv2
+import skimage
+from skimage.morphology import skeletonize
+
 from cv2.typing import MatLike
 from numpy.typing import NDArray
 
-from fingerprint_feature_extractor import extract_minutiae_features
 from fingerprint_enhancer import enhance_Fingerprint
+from fingerprint_feature_extractor import extract_minutiae_features
 
 
 class Fingerprint:
     def __init__(self, img: MatLike):
         self.img = img
         self.enhanced_img = enhance_Fingerprint(img)
-        self.terminations, self.bifurcations = extract_minutiae_features(img)
+        self.skeleton_img = np.uint8(skeletonize(self.enhanced_img)) * 255
+        self.terminations, self.bifurcations = extract_minutiae_features(
+            self.enhanced_img
+        )
+
+        self.result_img = cv2.cvtColor(self.skeleton_img, cv2.COLOR_GRAY2BGR)
+        for t in self.terminations:
+            (rr, cc) = skimage.draw.circle_perimeter(t.locX, t.locY, 3)
+            skimage.draw.set_color(self.result_img, (rr, cc), (0, 0, 255))
+        for b in self.bifurcations:
+            (rr, cc) = skimage.draw.circle_perimeter(b.locX, b.locY, 3)
+            skimage.draw.set_color(self.result_img, (rr, cc), (255, 0, 0))
 
 
 def main():
     img1 = cv2.imread("fingerprint1.bmp", cv2.IMREAD_GRAYSCALE)
-    # img2 = cv2.imread("fingerprint2.bmp", cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread("fingerprint2.bmp", cv2.IMREAD_GRAYSCALE)
     # img3 = cv2.imread("fingerprint3.bmp", cv2.IMREAD_GRAYSCALE)
 
-    print(img1.shape)
-
     fp1 = Fingerprint(img1)
-    print(fp1.terminations[0])
+    fp2 = Fingerprint(img2)
 
-    cv2.imshow("img1", img1)
-    cv2.imshow("img1 ridge segment", fp1.enhanced_img)
+    cv2.imshow("img1 result", fp1.result_img)
+    cv2.imshow("img2 result", fp2.result_img)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
